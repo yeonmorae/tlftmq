05d1447e6a4841b995f8ebe3aea58794a2f85c02
yeonmorae <pong3029@kyonggi.ac.kr>
2024-09-29 22:32:35 +0900

    first

---
+++ file1.py
@@ -0,0 +1,11 @@
+# 사용자로부터 두 정수 입력받기
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+# 합과 차 계산
+sum_result = num1 + num2
+difference_result = num1 - num2
+
+# 결과 출력
+print(f"두 정수의 합: {sum_result}")
+print(f"두 정수의 차: {difference_result}")
